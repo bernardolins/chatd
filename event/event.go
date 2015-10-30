0fda98bf9c8096fb7a75bc70ed6c419b60c6dbbb
Bernardo Lins <bernardolins28@gmail.com>
2015-10-30 00:25:36 -0200

    Create Event object

---
+++ event/event.go
@@ -0,0 +1,23 @@
+// Defines Event type and handles event communication between modules
+package event
+
+type E struct {
+	User    string
+	Action  string
+	Value   string
+	Channel string
+}
+
+func New(user string, action string, value string, channel string) *E {
+	e := new(E)
+	e.User = user
+	e.Action = action
+	e.Value = value
+	e.Channel = channel
+
+	return e
+}
+
+// Executes and instruction set by and event
+func Handle(event E) {
+}
