3f9d78a6cba6a5e8a29c63f2b32eaad58c2b5823
Bernardo Lins <bernardolins28@gmail.com>
2015-11-02 14:46:57 -0200

    Add concurrency to server with go routines

--- server/server.go
+++ server/server.go
@@ -13,6 +13,10 @@ type Server struct {
 }
 
 func (server *Server) Up(ip string, port byte) {
+	server.ip = ip
+	server.port = port
+
+	fmt.Println("Starting server on %s:%d", server.ip, server.port)
 	ln, err := net.Listen("tcp", ":9090")
 
 	if err != nil {
@@ -21,4 +25,23 @@ func (server *Server) Up(ip string, port byte) {
 	}
 
 	server.listener = ln
+}
+
+func (server *Server) Stop() {
+	server.listener.Close()
+}
+
+func (server *Server) Accept() {
+	conn, err := server.listener.Accept()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	go HandleConnection(conn)
+}
+
+func HandleConnection(conn net.Conn) {
+	//handle new connections
 }
